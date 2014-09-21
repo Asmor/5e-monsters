56aa3056ea7db5fbd8bdd019c0084bb42ce64b7a
asmor <itoltz@gmail.com>
2014-09-21 19:13:07 -0400

    Added encounter guidelines

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,6 +2,7 @@
 /* global alignments */
 /* global checkMonster */
 /* global crs */
+/* global levels */
 /* global monsters */
 /* global sourceFilters */
 /* global sources */
@@ -28,48 +29,94 @@ Controllers.main = {
 		$scope.sources = sources;
 		$scope.tags = Object.keys(tags).sort();
 		$scope.types = types;
+		$scope.levels = levels;
 
 		$scope.checkMonster = checkMonster;
 
-		$scope.encounter = {};
-		$scope.encounterQty = 0;
-		$scope.encounterExp = 0;
+		$scope.encounter = {
+			qty: 0,
+			exp: 0,
+			groups: {},
+			playerCount: 4,
+			partyLevel: $scope.levels[0],
+		};
+		$scope.encounter.qty = 0;
+		$scope.encounter.exp = 0;
 		$scope.addMonster = function (monster) {
-			$scope.encounter[monster.name] = $scope.encounter[monster.name] || {
+			$scope.encounter.groups[monster.name] = $scope.encounter.groups[monster.name] || {
 				qty: 0,
 				monster: monster,
 			};
 
-			$scope.encounter[monster.name].qty++;
-			$scope.encounterQty++;
-			$scope.encounterExp += monster.exp;
+			$scope.encounter.groups[monster.name].qty++;
+			$scope.encounter.qty++;
+			$scope.encounter.exp += monster.exp;
 		};
 		$scope.removeMonster = function (monster) {
-			$scope.encounter[monster.name].qty--;
-			$scope.encounterQty--;
-			$scope.encounterExp -= monster.exp;
-			if ( $scope.encounter[monster.name].qty === 0 ) {
-				delete $scope.encounter[monster.name];
+			$scope.encounter.groups[monster.name].qty--;
+			$scope.encounter.qty--;
+			$scope.encounter.exp -= monster.exp;
+			if ( $scope.encounter.groups[monster.name].qty === 0 ) {
+				delete $scope.encounter.groups[monster.name];
 			}
 		};
 		$scope.adjustedEncounterExp = function () {
-			var qty = $scope.encounterQty,
-				exp = $scope.encounterExp;
+			var qty = $scope.encounter.qty,
+				exp = $scope.encounter.exp,
+				multiplierCategory,
+				// 0 and 7 are repeats of 1 and 6, to handle large and small party adjustments
+				multipliers = [
+					1,
+					1,
+					1.5,
+					2,
+					2.5,
+					3,
+					4,
+					4,
+				];
 
 			if ( qty === 0 ) {
 				return 0;
 			} else if ( qty === 1 ) {
-				return exp;
+				multiplierCategory = 1;
 			} else if ( qty === 2 ) {
-				return Math.floor(exp * 1.5);
+				multiplierCategory = 2;
 			} else if ( qty < 7 ) {
-				return Math.floor(exp * 2);
+				multiplierCategory = 3;
 			} else if ( qty < 11 ) {
-				return Math.floor(exp * 2.5);
+				multiplierCategory = 4;
 			} else if ( qty < 15 ) {
-				return exp * 3;
+				multiplierCategory = 5;
 			} else {
-				return exp * 4;
+				multiplierCategory = 6;
+			}
+
+			if ( $scope.encounter.playerCount < 3 ) {
+				// Increase multiplier for parties of one and two
+				multiplierCategory++;
+			} else if ( $scope.encounter.playerCount > 5 ) {
+				// Decrease multiplier for parties of six through eight
+				multiplierCategory--;
+			}
+
+			return Math.floor(exp * multipliers[multiplierCategory]);
+		};
+		$scope.encounterDifficulty = function () {
+			var exp = $scope.adjustedEncounterExp(),
+				count = $scope.encounter.playerCount,
+				level = $scope.encounter.partyLevel;
+
+			if ( exp <= ( count * level.easy ) ) {
+				return "Easy";
+			} else if ( exp <= ( count * level.medium ) ) {
+				return "Medium";
+			} else if ( exp <= ( count * level.hard ) ) {
+				return "Hard";
+			} else if ( exp <= ( count * level.deadly ) ) {
+				return "Deadly";
+			} else {
+				return "Ludicrous";
 			}
 		};
 	},
--- scripts/global.js
+++ scripts/global.js
@@ -1,13 +1,14 @@
 /* exported Controllers */
 /* exported alignments */
 /* exported expByCr */
-/* exported tags */
+/* exported levels */
 /* exported monsters */
 /* exported monstersByName */
 /* exported registerSource */
+/* exported sizes */
 /* exported sourceFilters */
 /* exported sources */
-/* exported sizes */
+/* exported tags */
 /* exported types */
 "use strict";
 
@@ -149,6 +150,28 @@ var monsters = [],
 		"29": 135000,
 		"30": 155000,
 	},
+	levels = [
+		{ level: 1,		easy: 25,	medium: 50,		hard: 75,	deadly: 100 },
+		{ level: 2,		easy: 50,	medium: 100,	hard: 150,	deadly: 200 },
+		{ level: 3,		easy: 75,	medium: 150,	hard: 225,	deadly: 400 },
+		{ level: 4,		easy: 125,	medium: 250,	hard: 375,	deadly: 500 },
+		{ level: 5,		easy: 250,	medium: 500,	hard: 750,	deadly: 1100 },
+		{ level: 6,		easy: 300,	medium: 600,	hard: 900,	deadly: 1400 },
+		{ level: 7,		easy: 350,	medium: 750,	hard: 1100,	deadly: 1700 },
+		{ level: 8,		easy: 450,	medium: 900,	hard: 1400,	deadly: 2100 },
+		{ level: 9,		easy: 550,	medium: 1100,	hard: 1600,	deadly: 2400 },
+		{ level: 10,	easy: 600,	medium: 1200,	hard: 1900,	deadly: 2800 },
+		{ level: 11,	easy: 800,	medium: 1600,	hard: 2400,	deadly: 3600 },
+		{ level: 12,	easy: 1000,	medium: 2000,	hard: 3000,	deadly: 4500 },
+		{ level: 13,	easy: 1100,	medium: 2200,	hard: 3400,	deadly: 5100 },
+		{ level: 14,	easy: 1250,	medium: 2500,	hard: 3800,	deadly: 5700 },
+		{ level: 15,	easy: 1400,	medium: 2800,	hard: 4300,	deadly: 6400 },
+		{ level: 16,	easy: 1600,	medium: 3200,	hard: 4800,	deadly: 7200 },
+		{ level: 17,	easy: 2000,	medium: 3900,	hard: 5900,	deadly: 8800 },
+		{ level: 18,	easy: 2100,	medium: 4200,	hard: 6300,	deadly: 9500 },
+		{ level: 19,	easy: 2400,	medium: 4900,	hard: 7300,	deadly: 10900 },
+		{ level: 20,	easy: 2800,	medium: 5700,	hard: 8500,	deadly: 12700 },
+	],
 	i;
 
 crs.push({ text: "0", value: 0 });
