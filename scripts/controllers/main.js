634592c84d9550985aa535059d1ab4e56ad93595
asmor <itoltz@gmail.com>
2014-09-23 17:41:38 -0400

    Fixing CR filter ordering

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -18,7 +18,42 @@ Controllers.main = {
 	controller: function ($scope) {
 		window.scope = $scope;
 		$scope.alignments = alignments;
-		$scope.crInfo = crInfo;
+		$scope.crList = [
+			crInfo["0"],
+			crInfo["1/8"],
+			crInfo["1/4"],
+			crInfo["1/2"],
+			crInfo["1"],
+			crInfo["2"],
+			crInfo["3"],
+			crInfo["4"],
+			crInfo["5"],
+			crInfo["6"],
+			crInfo["7"],
+			crInfo["8"],
+			crInfo["9"],
+			crInfo["10"],
+			crInfo["11"],
+			crInfo["12"],
+			crInfo["13"],
+			crInfo["14"],
+			crInfo["15"],
+			crInfo["16"],
+			crInfo["17"],
+			crInfo["18"],
+			crInfo["19"],
+			crInfo["20"],
+			crInfo["21"],
+			crInfo["22"],
+			crInfo["23"],
+			crInfo["24"],
+			crInfo["25"],
+			crInfo["26"],
+			crInfo["27"],
+			crInfo["28"],
+			crInfo["29"],
+			crInfo["30"],
+		];
 		$scope.filters = {
 			source: sourceFilters,
 		};
