32945662978b69ea760e07f821c5f0f818433c57
asmor <itoltz@gmail.com>
2014-12-11 06:25:08 -0500

    Fixing battle-setup to use constants from require

--- modules/battle-setup/battle-setup.js
+++ modules/battle-setup/battle-setup.js
@@ -1,35 +1,35 @@
-/* global NO_MONSTERS */
-/* global NO_PLAYERS */
 "use strict";
 
-define({
-	url: "/battle-setup",
-	templateUrl: "modules/battle-setup/battle-setup.html",
-	controller: function ($scope, $state, actionQueue, combat, util) {
-		window.scope = $scope;
+define(["scripts/constants"], function (constants) {
+	return {
+		url: "/battle-setup",
+		templateUrl: "modules/battle-setup/battle-setup.html",
+		controller: function ($scope, $state, actionQueue, combat, util) {
+			window.scope = $scope;
 
-		$scope.partial = util.partialFactory("modules/battle-setup/partials/");
-		$scope.combat = combat;
+			$scope.partial = util.partialFactory("modules/battle-setup/partials/");
+			$scope.combat = combat;
 
-		var combatState = combat.init(),
-			forward;
+			var combatState = combat.init(),
+				forward;
 
-		if ( combatState & NO_PLAYERS ) {
-			actionQueue.unshift("players.manage", "You must select a party");
-			forward = true;
-		}
+			if ( combatState & constants.NO_PLAYERS ) {
+				actionQueue.unshift("players.manage", "You must select a party");
+				forward = true;
+			}
 
-		if ( combatState & NO_MONSTERS ) {
-			actionQueue.unshift("encounter-manager", "You must select an encounter");
-			forward = true;
-		}
+			if ( combatState & constants.NO_MONSTERS ) {
+				actionQueue.unshift("encounter-manager", "You must select an encounter");
+				forward = true;
+			}
 
-		if ( forward ) {
-			// In the end, send them back here
-			actionQueue.queue("battle-setup");
+			if ( forward ) {
+				// In the end, send them back here
+				actionQueue.queue("battle-setup");
 
-			actionQueue.next($state);
-			return;
-		}
-	},
+				actionQueue.next($state);
+				return;
+			}
+		},
+	};
 });
