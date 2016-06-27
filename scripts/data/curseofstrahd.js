817f242284ce30eecc761aca13ed5ec8c0df768a
olavmueller <olav.mueller@gmx.de>
2016-06-27 23:04:05 +0200

    "Curse of Strahd"
    And, last for today, the creatures from "Curse of Strahd"

---
+++ scripts/data/curseofstrahd.js
@@ -0,0 +1,27 @@
+"use strict";
+
+define({
+	name: "Curse of Strahd",
+	initialState: true,
+	contents: [
+		[ "f0502677-b3bc-45b5-9d2a-7372fddc1e34", 226 ], // Baba Lysaga's Creeping Hut
+		[ "e70abfb0-fa9f-4562-b65f-2fc63d4c2f9e", 226 ], // Broom of Animated Attack
+		[ "d7bd3f20-e8da-49dd-87f6-22c2ad01927f", 227 ], // Guardian Portrait
+		[ "c90192cf-fbf1-4bef-8794-f7d82bee36b5", 227 ], // Strahd's Animated Armor
+		[ "75e7baad-6bbc-45c2-bf63-5b9e13b4bebd", 228 ], // Baby Lysaga
+		[ "0f608e49-e8f8-4a97-9fbc-5cc989138b2c", 229 ], // Barovian Witch
+		[ "148e9550-d99f-4966-9179-00803048b7b9", 230 ], // Tree Blight
+		[ "a9a45b1e-1bdd-4e3c-ba90-001df9c3012d", 231 ], // Ezmerelda d'Avenir
+		[ "a80002c2-40b7-4dab-976f-bef0cfb514f7", 232 ], // Izek Strazni
+		[ "79c90ee5-0e3d-437c-90e0-e79b11dba0c1", 233 ], // Madam Eva
+		[ "2ff5978f-9383-4e56-8fe6-27961eedfe77", 234 ], // Mongrelfolk 
+		[ "e267fda0-3c81-48e2-b299-34c519fe74e1", 235 ], // Phantom Warrior
+		[ "fd46aa89-2747-4146-a6d3-e82acaeca48c", 236 ], // Pidlewick II
+		[ "6bf0d8cb-cd69-4e17-85b8-7b8b35b5e5b1", 237 ], // Rahadin
+		[ "40906ebd-8164-4827-9b90-0b40a0b653ff", 238 ], // Rictavio
+		[ "80ff81db-e64f-48e4-8750-1b8002dec83e", 240 ], // Strahd von Zarovich
+		[ "8c5b9532-9129-48bd-a7ba-2778dc429629", 241 ], // Strahd Zombie
+		[ "4565f845-09c8-4842-b298-ad3dd4055b53", 241 ], // Vladimir Horngaard
+		[ "2f23a622-a37b-4c8e-8e1c-a91b11ca6983", 242 ], // Wereraven
+	],
+});
