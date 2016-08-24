23b30d074f94dd1f84a2a6fd3f5a643360a95cc7
olavmueller <olav.mueller@gmx.de>
2016-08-24 23:24:47 +0200

    Tome of Beasts
    up to Dragon, Cave

---
+++ scripts/data/tomeofbeasts.js
@@ -0,0 +1,122 @@
+"use strict";
+
+define({
+	name: "Tome of Beasts",
+	initialState: true,
+	contents: [
+		[ "fcd7e583-025c-412e-a35f-7ba1c5843206", 8 ], // Aboleth, Nihileth
+		[ "49945869-1d13-4bdf-9f15-dffdc93aeeb4", 9 ], // Nihilethic Zombie
+		[ "1a633d85-3731-4a4d-8c9e-a536d9adfad6", 11 ], // Abominable Beauty
+		[ "3c3d5ba0-b614-4566-aaaa-95fc0c8d961f", 12 ], // Accursed Defiler
+		[ "3ce5021d-916a-443d-a310-c41dfd8a3580", 13 ], // Ala
+		[ "1e860d7b-4e49-4f53-81e0-7dba9c83555b", 14 ], // Algorith
+		[ "c642e6fe-042c-4286-b0f9-b150b31bc284", 15 ], // Alseid
+		[ "ced91f29-a25f-4964-838e-a870b9997862", 15 ], // Alseid Grovekeeper
+		[ "90ff1f35-d729-4844-abed-fd54471d6e8c", 16 ], // Amphiptere
+		[ "b2434c8b-600d-47ed-b3cd-4cc60718aa3f", 17 ], // Andrenjinyi
+		[ "d50661d0-ae1e-424e-a25e-6431db6bc3cf", 19 ], // Angatra
+		[ "5ae8fdbc-34b8-4a42-b737-72b0ac88111e", 20 ], // Chained Angel
+		[ "e4b1e894-6d70-43bb-afe7-8f51cd8a69a8", 21 ], // Fidele 
+		[ "7c81fe4e-ee03-46a9-92f4-51beef2e627f", 22 ], // Angler Worm
+		[ "ded2487d-834b-40c2-b5d1-a4a3fca49ed5", 23 ], // Giant Ant
+		[ "e7f62072-f6da-4e2f-98ab-d319a99f70e9", 23 ], // Giant Ant Queen
+		[ "e9e27407-0f34-477c-bf29-5b467dd8688d", 24 ], // Anubian
+		[ "0d31b96c-68c9-467c-b193-24ab298f9301", 25 ], // Arboreal Grappler
+		[ "5ebbc6d7-8854-4aa6-8aee-7ee021327a25", 26 ], // Aridni
+		[ "98620d9d-449e-4459-ab8b-328aeec551a7", 27 ], // Asanbosam
+		[ "b1a5f8d4-b1c1-4f4b-8079-3124445f3d56", 28 ], // Azza Gremlin
+
+		[ "267e5321-57ba-4bef-880e-95b1fadcaf2f", 29 ], // Baba Yagas Horsemen
+		[ "9024f9d0-bab8-4d68-8414-62550e8f2171", 31 ], // Bagiennik
+		[ "b28a0279-4cc4-4cec-a11a-4dc9afb93e55", 32 ], // Bastet Temple Cat
+		[ "13ccff69-326d-4414-984b-a7e5407eb71b", 33 ], // Bearfolk
+		[ "d4d6352d-b990-4c99-9f9d-ab5c5ba159a4", 34 ], // Behtu
+		[ "e301d09b-5c14-45cc-bc8c-c3a2489095b8", 35 ], // Beli
+		[ "9c8588ee-09b7-44fa-b772-2ea263f28e6c", 36 ], // Bereginyas
+		[ "af3a3b15-91d9-464d-85d7-abf95204a27a", 37 ], // Blemmyes
+		[ "f36499ae-b897-427c-87b5-c60d63c9f68b", 38 ], // Boloti
+		[ "5b6e8646-2883-4816-b406-6ffb609d3a48", 39 ], // Bone Collective
+		[ "f28d1e74-980d-4ef1-912a-68e23b3bbe95", 40 ], // Bone Crab 
+		[ "cdf3875f-178a-4a99-bcb5-15fabc681244", 41 ], // Bone Swarm
+		[ "12a75eb1-cddd-4af7-9afd-94d639bc40e1", 42 ], // Boreas
+		[ "83d71bdd-22d2-4206-8e5d-c6009f6d31ff", 44 ], // Bouda
+		[ "d32bb8f5-ec73-4b91-a5f6-266466720410", 45 ], // Broodiken
+		[ "9f5ab0a9-5c7c-4e6c-a26f-dc00b32c4523", 46 ], // Bucca
+		[ "4184047d-c775-4d83-8577-cac0bffa6a1c", 47 ], // Bukavac
+		[ "54c396e9-6b6d-4463-bf45-2e83a01e4ef5", 48 ], // Buraq
+		[ "89f9f934-37cb-40f2-b3c1-188df4a4a08e", 49 ], // Burrowling
+		
+		[ "674fd6b5-592f-4717-9b03-177c1cf4f5bf", 50 ], // Cactid
+		[ "e6f3dd42-c20b-495d-a760-a6f4d8505e99", 51 ], // Cambium
+		[ "b1c14b61-f2db-475c-b900-4d2ccc2a8fc1", 52 ], // Carrion Beetle
+		[ "0a40557f-8f49-47c7-823b-139cd49a4d2f", 53 ], // Cavelight Moss
+		[ "5cb25d80-ed86-4817-af6a-4d934b9a633b", 54 ], // Chelicerae
+		[ "8a8dfede-f0b5-4173-9aac-dd3bd9d08077", 55 ], // Chernomoi
+		[ "0f659c11-520c-4ce5-a957-d2409ccac868", 56 ], // Child of the Briar
+		[ "991dd272-dc07-47e6-8b5f-707d1099ceee", 57 ], // Chronalmental
+		[ "4faaaab2-447c-4fe1-a1b5-fc5487e4311f", 58 ], // Cikavak
+		[ "4af41011-a054-40e9-aae0-827aa8a7a198", 59 ], // Clockwork Abomination
+		[ "7ba732bf-d9ba-44e3-9ac7-db55bb55c17b", 60 ], // Clockwork Beetle
+		[ "c96276b1-d369-46d8-bf47-a18e630e4623", 61 ], // Clockwork Beetle Swarm
+		[ "69b8876f-11e3-4ff6-8c14-c7a238a1b3a5", 62 ], // Clockwork Hound
+		[ "8aa5470b-b117-4f0f-bda0-a1049f445268", 63 ], // Clockwork Huntsman
+		[ "bb046b76-72f1-4277-940e-2dbb87d73073", 64 ], // Clockwork Myrmidon
+		[ "1cd6c9c3-ddb4-4f51-ada9-479ed2751c23", 65 ], // Clockwork Watchman
+		[ "d80c84ff-8bce-40e8-b7a1-035b2cded742", 66 ], // Clockwork Weaving Spider
+		[ "c8ba631f-38db-4bf3-9188-d35cedc3fee5", 67 ], // Clurichaun
+		[ "da9a6770-9136-427d-afe6-6edcefe41b17", 68 ], // Cobbleswarm
+		[ "5df42798-f133-4df3-aea6-e249eed5f49d", 69 ], // Corpse Mound
+
+		[ "a8b87110-5394-4035-b4fb-27671b127a7d", 70 ], // Dau
+		[ "5d7b51f8-f1dc-4e42-a469-c44e38be0996", 71 ], // Death Butterfly Swarm
+		[ "217f088a-529d-42fd-a815-4880c393cbd5", 71 ], // Greater Death Butterfly Swarm
+		[ "9bfde490-5c91-4097-a6b0-b63427b1ab0a", 72 ], // Deathwisp
+		[ "c2c6bb2e-0372-42a9-a91d-a97b604f4ba2", 73 ], // Deep One
+		[ "8ca21423-f105-4240-9f58-8073e2579cdc", 73 ], // Deep One Hybrid Priest
+		[ "7b3d0676-429c-4e98-b1e4-2a30dc4c8a35", 74 ], // Deep One Archimandrite
+		[ "8d774866-4341-42c4-8679-b24b59fc6f5b", 75 ], // Apau Perape
+		[ "b1d88741-cd79-4752-bb6f-1ef696b7a9a4", 76 ], // Berstuc, Demon
+		[ "dc7c2dcc-7512-4743-ad9c-4700f63625a6", 77 ], // Kishi, Demon
+		[ "d321e41e-ddb9-449b-a03a-066d24566c14", 78 ], // Malakbel, Demon
+		[ "5394d8bb-772d-4a44-aac1-db6300bf4b9e", 79 ], // Psoglav, Demon
+		[ "bc46fd53-e06a-499a-b781-8ca3824b8b4e", 80 ], // Rubezahl, Demon
+		[ "85acd254-e004-4c15-91b3-990d1880575d", 82 ], // Akyishigal, Demon Lord of Cockroaches
+		[ "49b94afc-5003-4b15-8aac-09f4decdc506", 83 ], // Spawn of Akyishigal
+		[ "6618d8f9-4006-4a59-8519-db3fbd103d3a", 84 ], // Alquam, Demon Lord of Night
+		[ "4b6e0da3-3261-4d4e-a3c7-8873d4479070", 85 ], // Camazotz, Demon Lord of Bats and Fire
+		[ "d3568a44-6269-47cd-b213-35782c1f25b2", 87 ], // Skin Bat
+		[ "6ff39a6c-254a-44b9-9054-1b454266a7d5", 88 ], // Mechuiti, Demon Lord of Apes
+		[ "f53799a6-554d-4893-a48e-f66ab4577894", 90 ], // Qorgeth, Demon Lord of the Devouring Worm
+		[ "b9a861e7-02b9-4281-9703-876ee53b345d", 92 ], // Derro Fetal Savant
+		[ "e61dd711-d229-48d3-abe7-444efdb3f415", 93 ], // Derro Shadow Antipaladin
+		[ "d09d224e-f273-43b6-8d15-d7d302d859f0", 95 ], // Arbeyach
+		[ "c28ba1f4-62a3-4193-a571-9cd0fc9029d9", 97 ], // Spawn of Arbeyach
+		[ "afe75252-bdbf-490e-990e-0496b14fe729", 98 ], // Ia'Affrat
+		[ "3a75d531-eef3-4bec-a386-5c984ff90369", 99 ], // Mammon
+		[ "56cebeca-6791-4465-a8b0-04b96bc02d53", 101 ], // Totivillus, Scribe of Hell
+		[ "4f7e3706-2145-47d5-8367-6ba0381fd997", 102 ], // Automata Devil
+		[ "40d36b04-a33c-4b9a-84ed-a4506de964ad", 104 ], // Cohort Devil
+		[ "b46b05ee-656f-4588-b0a0-68322c3f8116", 105 ], // Crystalline Devil
+		[ "7b581c15-0da9-4718-a7da-ec08a140dc73", 106 ], // Gilded Devil
+		[ "7d7c18e6-c80b-483d-8302-ecb14033cc33", 107 ], // Ink Devil
+		[ "39d0b4b1-0025-4634-86c0-3bf717c91725", 108 ], // Koralk (Harvester Devil)
+		[ "66c123d3-a888-459c-bb7f-84d84ab90406", 110 ], // Lunar Devil
+		[ "c3a683d3-be46-4d12-95d4-a7bc4b0ce6df", 111 ], // Orobas Devil 
+		[ "30495e72-f1fd-4ee3-8ed3-c4c1492b50ff", 113 ], // Salt Devil
+		[ "4cc7e076-cde9-465a-865a-826fbfb772e8", 114 ], // Mbielu
+		[ "c2955321-3e1e-44b3-9d9c-fff7bbe0c1c8", 115 ], // Ngobou
+		[ "2fcdadab-7ad8-4a4a-9119-f3fd7c2222d7", 116 ], // Spinosaurus
+		[ "a2dbba10-4493-47d2-b83c-f3d84194d6a8", 117 ], // Young Spinosaurus
+		[ "029b2de7-9340-480f-b6af-ff0977938314", 118 ], // Dipsa
+		[ "136623a6-3663-4fe4-b735-a76dc2555bac", 119 ], // Dissimortuum
+		[ "743a4dd7-3466-473a-b63a-e2a20202a911", 120 ], // Dogmole
+		[ "1f9e3430-2364-4963-951c-293834918685", 121 ], // Dogmole Juggernaut
+		[ "1a197bad-659e-46b1-a068-c8431a34b6f1", 122 ], // Domovoi
+		[ "4ccf6f2d-e76f-4ee6-aff2-95fbcea22886", 123 ], // Doppelrat
+		[ "9b159862-bb1d-4d99-9088-67b06781af2b", 124 ], // Dorreq
+		[ "40aded6f-4cd4-4a24-969e-7d68648e9ac2", 125 ], // Adult Cave Dragon
+		[ "c7d443c2-ed3c-4015-9e9e-7b11e15e0e9d", 127 ], // Young Cave Dragon
+		[ "307aaf51-a949-4afd-909d-d6c31680802b", 127 ], // Cave Dragon Wyrmling
+		
+	],
+});
