ea6047fb628c510c201adc886deaf58fa769d362
Joe Barzilai <joe.barzilai@asynchrony.com>
2016-12-24 21:23:47 -0600

    Cleanup for consistency and tests

--- app/encounter-builder/encounter-builder.controller.js
+++ app/encounter-builder/encounter-builder.controller.js
@@ -9,16 +9,48 @@
 	function EncounterBuilderController($scope, $log, store, actionQueue, encounter, monsters, sources) {
 		var vm = this;
 
-		// There's no way to tell when they're done building an encounter, so clear the queue if they ever make it here.
-		actionQueue.clear();
-
-		vm.filters = {
-			source: sources.filters,
-			pageSize: 10,
-		};
 		vm.encounter = encounter;
+		vm.getMonsterQtyString = getMonsterQtyString;
 
-		vm.getMonsterQtyString = function () {
+		activate();
+
+		function activate() {
+			// There's no way to tell when they're done building an encounter, so clear the queue if they ever make it here.
+			actionQueue.clear();
+
+			vm.filters = {
+				source: sources.filters,
+				pageSize: 10,
+			};
+
+			$scope.$watch(function (scope) { return vm.encounter.groups; }, function (newValue, oldValue) {
+				var subtotal = 0;
+
+				_.forEach(newValue, function(item, idx) {
+					var groupQty = item.qty;
+					var groupMonster = monsters.byId[idx];
+
+					subtotal += groupMonster.cr.exp * groupQty;
+				});
+
+				vm.encounter.exp = subtotal;
+			}, true);
+
+			store.get("5em-filters").then(function (frozen) {
+				if (frozen) {
+					$log.log('Thaw filters');
+					vm.filters = frozen;
+				}
+			})
+			.finally(function() {
+				$scope.$watch("vm.filters", function () {
+					$log.log('Freeze filters');
+					store.set("5em-filters", vm.filters);
+				}, true);
+			});
+		}
+
+		function getMonsterQtyString() {
 			var qty = Object.keys(vm.encounter.groups).reduce(function (sum, key) {
 				return sum + vm.encounter.groups[key].qty;
 			}, 0);
@@ -29,31 +61,5 @@
 
 			return qty + " enemies";
 		};
-
-		$scope.$watch(function (scope) { return vm.encounter.groups; }, function (newValue, oldValue) {
-			var subtotal = 0;
-
-			_.forEach(newValue, function(item, idx) {
-				var groupQty = item.qty;
-				var groupMonster = monsters.byId[idx];
-
-				subtotal += groupMonster.cr.exp * groupQty;
-			});
-
-			vm.encounter.exp = subtotal;
-		}, true);
-
-		$log.log('Thaw filters');
-		store.get("5em-filters").then(function (frozen) {
-			if (frozen) {
-				vm.filters = frozen;
-			}
-		})
-		.finally(function() {
-			$scope.$watch("vm.filters", function () {
-				$log.log('Freeze filters');
-				store.set("5em-filters", vm.filters);
-			}, true);
-		});
 	}
 })();
