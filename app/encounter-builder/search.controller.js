e3d30f4157cdcf91ba06486f2caa29ec954d0e85
asmor <itoltz@gmail.com>
2017-03-26 03:42:02 -0400

    Adding custom sources works, and pared down buttons in sources modal

--- app/encounter-builder/search.controller.js
+++ app/encounter-builder/search.controller.js
@@ -21,6 +21,11 @@
 
         activate();
 
+        $scope.$on("custom-source-added", function (event, sourceName) {
+            // Custom content should be enabled by default
+            vm.filters.source[sourceName] = true;
+        });
+
         ////////////////
 
         function activate() {
@@ -33,57 +38,31 @@
             vm.filters.minCr = null;
             vm.filters.maxCr = null;
             vm.filters.environment = null;
-        };
+        }
 
         function updateSourceFilters(newValue) {
+            console.log("TODO", newValue);
             if (newValue) {
                 vm.filters.sources = newValue;
             }
+
             // The default is core, but for implementation reasons it's represented by the empty string
-            var sourceTypes = vm.filters.sources || "core",
-                select = [ ],
-                i;
+            var sourceTypes = vm.filters.sources || "core";
 
-            if ( sourceTypes === "custom" ) {
-                return;
+            // If we're selecting everything, turn everything on. Otherwise turn everything off and
+            // then selectively turn things on
+            sources.all.forEach(function (sourceName) {
+                vm.filters.source[sourceName] = (sourceTypes === "all");
+            });
+            if ( sourceTypes === "core" ) {
+                [
+                    "Player's Handbook",
+                    "Monster Manual",
+                    "Volo's Guide to Monsters",
+                ].forEach(function (sourceName) {
+                    vm.filters.source[sourceName] = true;
+                });
             }
-
-            if ( sourceTypes.match(/all|core|books/) ) {
-                select.push("Player's Handbook");
-                select.push("Monster Manual");
-                select.push("Volo's Guide to Monsters");
-            }
-
-            if ( sourceTypes.match(/all|books/) ) {
-                select.push("Hoard of the Dragon Queen");
-                select.push("Rise of Tiamat");
-                select.push("Princes of the Apocalypse");
-                select.push("Out of the Abyss");
-                select.push("Curse of Strahd");
-                select.push("Storm King's Thunder"); 
-            }
-
-            if ( sourceTypes.match(/all|basic/) ) {
-                select.push("Basic Rules v1");
-                select.push("HotDQ supplement");
-                select.push("Princes of the Apocalypse Online Supplement v1.0");
-            }
-            
-            if ( sourceTypes.match(/all|thirdparty/) ) {
-                select.push("Monster-A-Day");
-                select.push("Fifth Edition Foes");
-                select.push("Primeval Thule Campaign Setting");
-                select.push("Primeval Thule Gamemaster's Companion");
-                select.push("Tome of Beasts");
-            }
-
-            for ( i = 0; i < sources.all.length; i++ ) {
-                vm.filters.source[sources.all[i]] = false;
-            }
-
-            while (select.length) {
-                vm.filters.source[select.pop()] = true;
-            }
-        };
+        }
     }
 })();
--- app/services/monsters.service.js
+++ app/services/monsters.service.js
@@ -5,24 +5,38 @@
 
 	// https://docs.google.com/spreadsheets/d/19ngAA7d1eYKiBtKTsg8Qcsq_zhDSBzEMxXS45eCdd7I/edit
 	var masterSheetId = "19ngAA7d1eYKiBtKTsg8Qcsq_zhDSBzEMxXS45eCdd7I";
+	var all = [];
+	var byId = {};
+	var byCr = {};
+	var loaded = {};
 
-	Monsters.$inject = [
-		"googleSheetLoader",
-		"misc",
-		"monsterFactory",
-	];
+	Monsters.$inject = ["$rootScope", "googleSheetLoader", "misc", "monsterFactory"];
+	function Monsters($rootScope, googleSheetLoader, miscLib, monsterFactory) {
+		function loadSheet(sheetId, custom) {
+			if ( loaded[sheetId] ) {
+				// Don't allow a source to be loaded multiple times
+				return;
+			}
 
-	function Monsters(
-		googleSheetLoader,
-		miscLib,
-		monsterFactory
-	) {
-		var registedSources = {};
-		var all = [];
-		var byId = {};
-		var byCr = {};
+			loaded[sheetId] = true;
 
-		googleSheetLoader(masterSheetId)
+			loadMonsters($rootScope, googleSheetLoader, miscLib, monsterFactory, sheetId, custom);
+		}
+
+		loadSheet(masterSheetId);
+		window.loadSheet = loadSheet;
+
+		return {
+			all: all,
+			byCr: byCr,
+			byId: byId,
+			check: monsterFactory.checkMonster,
+			loadSheet: loadSheet,
+		};
+	}
+
+	function loadMonsters($rootScope, googleSheetLoader, miscLib, monsterFactory, sheetId, custom) {
+		googleSheetLoader(sheetId)
 		.then(function (sheets) {
 			sheets.Monsters.forEach(function (monsterData) {
 				var monster = new monsterFactory.Monster(monsterData);
@@ -42,29 +56,22 @@
 			sheets.Sources.forEach(function (sourceData) {
 				var name = sourceData.name;
 				var shortName = sourceData.shortname;
-				var initialState = !!(sourceData.defaultselected || "").match(/yes/i);
-
-				if ( registedSources[name] ) {
-					return;
-				}
-
-				registedSources[name] = true;
+				var initialState = custom || !!(sourceData.defaultselected || "").match(/yes/i);
 
 				miscLib.sources.push(name);
 				miscLib.sourceFilters[name] = initialState;
 				miscLib.shortNames[name] = shortName;
+
+				if ( custom ) {
+					$rootScope.$broadcast("custom-source-added", name);
+				}
 			});
+
+			miscLib.sources.sort();
 
 			all.sort(function (a, b) {
 				return (a.name > b.name) ? 1 : -1;
 			});
 		});
-
-		return {
-			all: all,
-			byCr: byCr,
-			byId: byId,
-			check: monsterFactory.checkMonster,
-		};
 	}
 })();
