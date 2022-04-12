60fc8e5afd080468b5b32ae3a27e3f04ee83c358
V13Axel <alex@stechstudio.com>
2022-04-12 17:20:07 -0400

    Dark theme brah

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     content: ["src/**/*.{html,js}"],
+    darkMode: 'class',
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            }
+        },
     },
     plugins: [
         require('@tailwindcss/forms')
